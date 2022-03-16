e740531850cb4b268c608c3bec535ac410b0f323
t <t>
2022-03-16 13:56:30 +0000

    Changed the index file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const chalk = require("chalk");
 
 const colleagues = [
-  { name: "Nuno", color: "blue" },
-  { name: "whiteStone", color: "red" },
-  { name: "Patricia", color: "yellow" },
-  { name: "Chicote", color: "green" },
-];
+
+	 {name: "Nuno", color: "blue"} ,
+	 {name: "whiteStone", color: "red"} ,
+	 {name: "Patricia", color: "yellow"} ,
+	 {name: "Chicote", color: "green"}
+]
 
 for (i = 0; i < colleagues.length; i++) {
-  const { name, color } = colleagues[i];
-  console.log(chalk[color](name));
+	console.log(chalk[colleagues[i].color](colleagues[i].name));
 }
+
+
